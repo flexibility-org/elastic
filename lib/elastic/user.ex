be43e897558da8ce14eb4971078309ab273f9e4a
Oleks <oleks@oleks.info>
2021-04-26 12:06:30 +0200

    Add capability to create and delete users

--- lib/elastic/user.ex
+++ lib/elastic/user.ex
@@ -1,6 +1,49 @@
 defmodule Elastic.User do
+  alias Elastic.HTTP
+
+  @base_url "/_security/user/"
+
+  @doc """
+    URL-encode username for use with the Elastic Users API.
+
+    In this API, the username is supplied as part of a URL path, and
+    yet, Elastic allows for all sorts of printable ASCII characters in
+    usernames, including ?, /, &, =, and so on.
+
+    This percent-encodes everything but the unreserved URL character
+    set, as defined by [RFC 3986, section
+    2.3](https://tools.ietf.org/html/rfc3986#section-2.3). In particular,
+    everything, but the following character is percent-encoded:
+
+    * Alphanumeric ASCII characters: `A-Z`, `a-z`, and `0-9`.
+    * `~`, `_`, `-`, `.`
+  """
+  @spec url_encode_username(username :: binary()) :: binary()
+  defp url_encode_username(username) do
+    URI.encode(username, &URI.char_unreserved?/1)
+  end
+
+  @spec create(
+          username :: binary(),
+          password :: binary()
+        ) :: ResopnseHandler.result()
+  def create(username, password) do
+    HTTP.put(@base_url <> url_encode_username(username),
+      body: %{
+        password: password,
+        roles: []
+      }
+    )
+  end
+
+  @spec delete(username :: binary()) :: ResponseHandler.result()
+  def delete(username) do
+    HTTP.delete(@base_url <> url_encode_username(username))
+  end
+
   def is_valid_username?(username) do
     len = String.length(username)
+
     len > 0 && len <= 1024 &&
       Regex.run(~r{^[!-~][ -~]+[!-~]$}, username) != nil
   end
---
+++ test/elastic/integration/user_test.exs
@@ -0,0 +1,30 @@
+defmodule Elastic.Integration.UserTest do
+  use ExUnit.Case, async: true
+  use ExUnitProperties
+
+  alias Elastic.User
+
+  @valid_non_infix_chars 0x21..0x7E
+
+  def valid_username_gen do
+    gen all(
+          text <- string(0x20..0x7E, min_length: 1, max_length: 341),
+          prefix <- string(@valid_non_infix_chars, min_length: 1, max_length: 341),
+          postfix <- string(@valid_non_infix_chars, min_length: 1, max_length: 341)
+        ) do
+      prefix <> text <> postfix
+    end
+  end
+
+  @tag integration: true
+
+  property "basic create/2 and delete/1" do
+    check all(
+            username <- valid_username_gen(),
+            max_runs: 10
+          ) do
+      assert User.create(username, "password") == {:ok, 200, %{"created" => true}}
+      assert User.delete(username) == {:ok, 200, %{"found" => true}}
+    end
+  end
+end
