c9298769d85e3f72b2bc0d8568ba85982a56a73f
Oleks <oleks@oleks.info>
2021-04-27 16:47:02 +0200

    Configure stream_data explicitly on dev and test

--- config/dev.exs
+++ config/dev.exs
@@ -4,3 +4,9 @@ config :elastic,
   index_prefix: "elastic",
   use_mix_env: true,
   basic_auth: {"elastic", "password"}
+
+config :stream_data,
+  initial_size: 1,
+  max_runs: 100,
+  max_run_time: :infinity,
+  max_shrinking_steps: 100
